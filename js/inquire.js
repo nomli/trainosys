f2da2ccadcce8404cad7975192a4e087cfcde341
GBS\norman.omli <norman@tirewebmarketing.com>
2019-03-01 13:07:48 +0800

    no message

--- js/inquire.js
+++ js/inquire.js
@@ -1,22 +1,26 @@
 (function(d){
 	var a = d.querySelectorAll("#courses ul>li"),
-		  trainingInq = jQuery("#trainingInq");
+		  trainingInq = jQuery("#trainingInq"),
+			wpcf7 = jQuery("#wpcf7-form"),
+			cf7res = jQuery("#cf7res"),
+			wpcf7responseoutput = jQuery('.wpcf7-response-output');
+	
+	wpcf7responseoutput.appendTo(wpcf7);
 	
 	[].forEach.call(a,function(e){
 			e.addEventListener("click",function(){
 				trainingInq.modal();
 			})
 	});
-	document.addEventListener( 'wpcf7mailsent', function( event ) {
-		trainingInq.modal("hide");
-		
-		setTimeout(function(){
-			$('html, body').animate({
-				scrollTop:  $(document).height()
-			}, 800);
-		},600)
-	}, false );
-	document.addEventListener( 'wpcf7invalid', function( event ) {
-		//return false;
-	}, false );
+	document.addEventListener( 'wpcf7submit', function( event ) {
+		jQuery('.wpcf7-response-output').appendTo("#cf7res");
+		if(event.detail.status == "validation_failed"){
+			event.preventDefault();
+		}else{
+			setTimeout(function(){
+				trainingInq.modal("hide");
+			},2000);
+		}
+		return false;
+	}, false );	
 })(document);
